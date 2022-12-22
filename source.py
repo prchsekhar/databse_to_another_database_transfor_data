1bc4c401f141c81306892b8c31795009db7d5295
prchsekhar <polamarasetti.rajachandrasekhar@3i-infotech.com>
2022-12-22 15:44:24 +0530

    basic code

---
+++ source.py
@@ -0,0 +1,37 @@
+import sys
+from loguru import logger
+
+from utility import get_tables, load_db_details
+from read import read_table
+from write import load_table
+
+
+def main():
+    env = sys.argv[1]
+    a_tables = sys.argv[2]
+    logger.add("data-copier.info",
+               rotation="1 MB",
+               retention="10 days",
+               level="INFO"
+               )
+    logger.add("data-copier.err",
+               rotation="1 MB",
+               retention="10 days",
+               level="ERROR"
+               )
+    db_details = load_db_details(env)
+    tables = get_tables('table_list', a_tables)
+    for table_name in tables['table_name']:
+        logger.info(f'reading data for {table_name}')
+        data, column_names = read_table(db_details, table_name)
+        logger.info(f'loading data for {table_name}')
+        load_table(db_details, data, column_names, table_name)
+
+
+def truncate_tables(env, tables):
+    db_details = load_db_details(env)
+
+
+
+if __name__ == '__main__':
+    main()
